0f785cfb1835c4fea999c36e45ae849f2290b344
Leland Krych <leland.krych@gmail.com>
2020-01-02 10:26:31 -0800

    remove space

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-
 class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'db/climbing_stats.db')
