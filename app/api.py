3f6ffd17ab43d5ddc080186167f7b4e5af004821
Leland Krych <leland.krych@gmail.com>
2020-01-01 21:31:49 -0800

    move api into app

---
+++ app/api.py
@@ -0,0 +1,36 @@
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
+from app.models import User, Workout
+
+#initialize db
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     if request.method == 'POST':
+#         return do_the_login()
+#     else:
+#         return show_the_login_form()
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    user = User.create_user(request.data)
+    return user
+
+@app.route('/user/<id>')
+def get_user():
+    user = User.get_user(id)
+    return user
+
+@app.route('/workouts', methods=['POST'])
+def create_workout():
+    workout = Workout.create_workout(request.data)
+    return workout
+
+@app.route('/workout/<id>')
+def get_workout():
+    workout = Workout.get_workout(id)
+    return workout
