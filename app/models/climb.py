c7d431e1a8bb551481a1e6ffa106ffc7d7c8d682
Leland Krych <leland.krych@gmail.com>
2020-01-02 10:31:07 -0800

    hopefully have models working correctly

--- app/models/climb.py
+++ app/models/climb.py
@@ -1,12 +1,12 @@
 from app import db
 
-class Climb(db.Model):
+class Climbs(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     type = db.Column(db.Integer)
     grade = db.Column(db.Integer)
     letter_grade = db.Column(db.Integer)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    workout_id = db.Column(db.Integer, db.ForeignKey('workout.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    workout_id = db.Column(db.Integer, db.ForeignKey('workouts.id'))
 
     def __repr__(self):
         return '<Workout {}>'.format(self.id)
--- app/models/workout.py
+++ app/models/workout.py
@@ -1,12 +1,12 @@
 from datetime import datetime
 from app import db
 
-class Workout(db.Model):
+class Workouts(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.String(128), default=datetime.timestamp(datetime.now()))
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    climbs = db.relationship('Climb',
-        backref=db.backref('workout', lazy=True))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    # climbs = db.relationship('climb',
+    #     backref=db.backref('workout', lazy=True))
 
     def __repr__(self):
         return '<Workout {}>'.format(self.id)
