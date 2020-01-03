52cb0b43159f59edd7288e118583ee361a919258
Leland Krych <leland.krych@gmail.com>
2020-01-02 21:44:45 -0800

    add some error handling and remove to_json calls from routes

--- app/routes.py
+++ app/routes.py
@@ -14,15 +14,29 @@ def hello_world():
 #     else:
 #         return show_the_login_form()
 
+### USER ROUTES ###########
+
 @app_instance.route('/users', methods=['POST'])
 def create_user():
     user = helpers.create_user(request.get_json())
-    return user.to_json()
+    return user
 
 @app_instance.route('/user/<user_id>')
 def get_user(user_id):
     user = helpers.get_user(user_id)
-    return user.to_json()
+    return user
+
+@app_instance.route('/user/<user_id>', methods=['PUT', 'PATCH'])
+def update_user(user_id):
+    user = helpers.update_user(user_id, request.get_json())
+    return user
+
+@app_instance.route('/user/<user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    user = helpers.delete_user(user_id)
+    return user.id
+
+### WORKOUT ROUTES ###########
 
 @app_instance.route('/user/<user_id>/workouts', methods=['POST'])
 def create_workout(user_id):
@@ -31,7 +45,7 @@ def create_workout(user_id):
         try:
             workout = helpers.create_workout(user_id, request.get_json())
             return workout
-        except AssertionError as e:
+        except Exception as e:
             print(e)
             return jsonify({"message": str(e)}), 400
     else:
@@ -43,8 +57,40 @@ def create_workout(user_id):
 def get_workout(user_id, workout_id):
     user_exists = helpers.check_if_user_exists(user_id)
     if user_exists:
-        workout = helpers.get_workout(workout_id)
-        return workout.to_json()
+        try:
+            workout = helpers.get_workout(workout_id)
+            return workout
+        except Exception as e:
+            print(e)
+            return jsonify({"message": str(e)}), 400
+    else:
+        helpers.user_dne_exception()
+        return jsonify(), 400
+
+@app_instance.route('/user/<user_id>/workout/<workout_id>', methods=['PUT', 'PATCH'])
+def update_workout(user_id, workout_id):
+    user_exists = helpers.check_if_user_exists(user_id)
+    if user_exists:
+        try:
+            workout = helpers.update_workout(workout_id, request.get_json())
+            return workout
+        except Exception as e:
+            print(e)
+            return jsonify({"message": str(e)}), 400
+    else:
+        helpers.user_dne_exception()
+        return jsonify(), 400
+
+@app_instance.route('/user/<user_id>/workout/<workout_id>', methods=['DELETE'])
+def delete_workout(user_id, workout_id):
+    user_exists = helpers.check_if_user_exists(user_id)
+    if user_exists:
+        try:
+            workout = helpers.delete_workout(workout_id)
+            return workout.id
+        except Exception as e:
+            print(e)
+            return jsonify({"message": str(e)}), 400
     else:
         helpers.user_dne_exception()
         return jsonify(), 400
