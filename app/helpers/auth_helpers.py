4a4fa7d50f2f9af88aed559b90718fd39c71ff8e
Leland Krych <leland.krych@gmail.com>
2020-01-03 12:55:10 -0800

    use new factory helper methods

--- app/helpers/auth_helpers.py
+++ app/helpers/auth_helpers.py
@@ -1,4 +1,4 @@
-from app import bcrypt
+from app.helpers import factory_helpers
 from app.helpers import model_helpers
 
 # used to authenticate user
@@ -6,7 +6,7 @@ from app.helpers import model_helpers
 # even though it returns the user it will only use the users id in the JWT
 def authenticate(username, password):
     user = model_helpers.get_user_by_username(username)
-    if user and bcrypt.check_password_hash(user.password_hash, password):
+    if user and factory_helpers.bcrypt.check_password_hash(user.password_hash, password):
         return user
 
 # used when we receive a JWT
