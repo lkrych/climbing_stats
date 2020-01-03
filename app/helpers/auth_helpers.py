799924a1e006781d2dd700427dc168f7c8b3185d
Leland Krych <leland.krych@gmail.com>
2020-01-03 10:31:12 -0800

    Add some helpful comments about auth methods

--- app/helpers/auth_helpers.py
+++ app/helpers/auth_helpers.py
@@ -1,12 +1,17 @@
 from app import bcrypt
 from app.helpers import model_helpers
 
+# used to authenticate user
+# a post to the /auth endpoint routes to this function
+# even though it returns the user it will only use the users id in the JWT
 def authenticate(username, password):
-    print(username)
     user = model_helpers.get_user_by_username(username)
     if user and bcrypt.check_password_hash(user.password_hash, password):
-        return user #even though it returns the user it will only use the users id in the JWT
+        return user
 
+# used when we receive a JWT
+# unpacks JWT and gives payload data to identity
+# the payload contains the user's id
 def identity(payload):
     user_id = payload['identity']
     return model_helpers.get_user(user_id)
