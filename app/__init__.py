3193dda41056ecee5c4063be5749202b1f5d1732
Leland Krych <leland.krych@gmail.com>
2020-01-03 12:54:19 -0800

    use new factory function pattern in creation of application

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,25 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
 from flask_jwt import JWT
+import os
 
-from config import Config
+from config import app_config
 
+#application factory function - http://flask.palletsprojects.com/en/1.1.x/tutorial/factory/
+# https://flask.palletsprojects.com/en/1.1.x/patterns/appfactories/
 
-app_instance = Flask(__name__)
-app_instance.config.from_object(Config)
-db = SQLAlchemy(app_instance)
-bcrypt = Bcrypt(app_instance)
+def create_app():
+    #create and configure app
+    app_instance = Flask(__name__)
+    app_instance.config.from_object(app_config[os.environ.get('FLASK_ENV')])
 
-# hack for resolving circular import between auth_helpers and this file
-from app.helpers import auth_helpers
-jwt = JWT(app_instance, auth_helpers.authenticate, auth_helpers.identity)
+    from app.helpers import factory_helpers
 
-from app import routes
+    factory_helpers.db.init_app(app_instance)
+    factory_helpers.bcrypt.init_app(app_instance)
+    # hack for resolving circular import between auth_helpers and this file
+    from app.helpers import auth_helpers
+    jwt = JWT(app_instance, auth_helpers.authenticate, auth_helpers.identity)
+    
+    from app.routes import route_blueprint
+    app_instance.register_blueprint(route_blueprint)
+    return app_instance
