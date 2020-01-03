489cec425cba1327feef1ddf29789e0d6dc78815
Leland Krych <leland.krych@gmail.com>
2020-01-03 10:15:49 -0800

    Add jwt support to app init

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_jwt import JWT
 
 from config import Config
 
 
-#initialize db
 app_instance = Flask(__name__)
 app_instance.config.from_object(Config)
 db = SQLAlchemy(app_instance)
 bcrypt = Bcrypt(app_instance)
+
+# hack for resolving circular import between auth_helpers and this file
+from app.helpers import auth_helpers
+jwt = JWT(app_instance, auth_helpers.authenticate, auth_helpers.identity)
 
 from app import routes
