44edee56d969825de6bcd4fdcef95481c75eef9e
Tatiana Faramarzi <tfaramarzi@gmail.com>
2020-04-04 14:09:32 -0700

    Add counter to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-export default () => (
-  <>
-    <h1>Welcome to React Parcel Micro App!</h1>
-    <p>Hard to get more minimal than this React app.</p>
-  </>
-);
+export default () => {
+
+  let [count, setCount] = useState(0);
+
+
+
+  return (
+    <>
+      <h1>Welcome to React Parcel Micro App!</h1>
+      <p>Hard to get more minimal than this React app.</p>
+      <p>Counter: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </>
+  ) 
+};
