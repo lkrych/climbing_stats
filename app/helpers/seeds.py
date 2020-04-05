8b339516b7fdaccd811b4ec412eed792a17bee49
Leland Krych <leland.krych@gmail.com>
2020-04-04 17:42:52 -0700

    fix bugs I found in seeds.py

--- app/helpers/seeds.py
+++ app/helpers/seeds.py
@@ -1,38 +1,58 @@
+import click
+from flask.cli import with_appcontext
+import datetime
+from random import randrange
+
+from app.helpers.factory_helpers import db, bcrypt
 from app.models.user import Users
 from app.models.workout import Workouts
 from app.models.climb import Climbs
 
-def seed_database(db, users = 1, workouts = 50):
-    print("==== seeding the database =====")
+@click.command()
+@with_appcontext
+def seed_db(users = 1, workouts = 50):
+    """Seed Database"""
     for user in range(users):
-        u = create_user(db)
+        u = create_user()
+        print("User: {}".format(u.id))
         for workout in range(workouts):
-            create_workout(db, u.id)
+            create_workout(u.id)
+
+@click.command()
+@with_appcontext
+def reset_db():
+    """Remove all items from DB"""
+    Users.query.delete()
+    Workouts.query.delete()
+    Climbs.query.delete()
     db.session.commit()
 
-
-def create_user(db):
+def create_user():
     u = Users(username='blahblah',
         email='blah@example.com',
         password_hash=bcrypt.generate_password_hash('blahblah').decode('utf-8'))
     db.session.add(u)
+    db.session.commit()
     return u
 
-def create_workout(db, user_id, climbcount = 20):
+def create_workout(user_id, climbcount = 20):
     workout = Workouts(
         date = random_date(),
         user_id = user_id
     )
     db.session.add(workout)
+    db.session.commit()
+
     climbs = []
     for b in range(climbcount // 2):
-        climbs.append(create_boulders(user_id, workout_id))
+        climbs.append(create_boulders(user_id, workout.id))
     
     for r in range(climbcount // 2):
-        climbs.append(create_routes(user_id, workout_id))
+        climbs.append(create_routes(user_id, workout.id))
 
     for climb in climbs:
         db.session.add(climb)
+        db.session.commit()
 
 def create_boulders(user_id, workout_id):
     c = Climbs(
@@ -52,7 +72,7 @@ def create_routes(user_id, workout_id):
     )
     return c
 
-def random_date(day, month):
+def random_date():
     #random between jan-mar
     d = datetime.datetime(2020, randrange(1,4), randrange(1, 29), 18, 00)
-    return datetime.timestamp(d)
+    return datetime.datetime.timestamp(d)
