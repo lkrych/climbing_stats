b847d90899818d7547c10fc26887e546fbc35668
Leland Krych <leland.krych@gmail.com>
2020-04-04 22:53:02 -0700

    create signup

---
+++ frontend/src/components/Signup.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { postRequest } from "../util/request";
+
+export default () => {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const submitSignup = (e) => {
+        e.preventDefault();
+        postRequest("/users",
+            {
+                username,
+                email,
+                password
+            }
+        ).then((json) => {
+            console.log(json);
+        });
+    }
+
+    return (
+        <form onSubmit={(e) => submitSignup(e)}>
+            <label>Username: </label>
+            <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+            <br />
+            <label>Email: </label>
+            <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+            <br />
+            <label>Password: </label>
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            <button type="submit"> Sign Up </button>
+        </form>
+    )
+};
