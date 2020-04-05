4f3bd1b005d33e47f29dd572b61d7873bbca8425
Leland Krych <leland.krych@gmail.com>
2020-04-04 22:45:14 -0700

    set up signup and move state out of nav

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,18 @@ import React, { useState } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Nav from "./components/Nav";
+import Signup from "./components/Signup";
 
 export default () => {
   const [loggedIn, setLoggedIn] = useState(false);
+  const [showSignup, setShowSignup] = useState(false);
 
   return (
     <BrowserRouter>
-      <Nav loggedIn={loggedIn}/>
+      <Nav loggedIn={loggedIn} showSignup={showSignup} setShowSignup={setShowSignup}/>
       <Switch>
         <Route>
-
+            <Signup />
         </Route>
       </Switch>
     </BrowserRouter>
