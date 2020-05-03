678e364a3c8ab7225e8d56c17259da01718e692f
Tatiana Faramarzi <tfaramarzi@gmail.com>
2020-05-02 20:02:01 -0700

    Bring histogram into dashboard

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,9 +1,13 @@
 import React from "react";
 
+import Histogram from "./Histograms";
+
 export default () => {
     return (
         <div>
             <h1>Dashboard coming here</h1>
+            {/* Dropdown should send one of the following query params: "week" for week, "month" for month, "year" for year */}
+            <Histogram />
         </div>    
     )
 };
