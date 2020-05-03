8bc0e775eb4309c89af9511ec65794e581d29d1f
Tatiana Faramarzi <tfaramarzi@gmail.com>
2020-05-02 21:32:02 -0700

    Set up routes histogram

--- frontend/src/components/Histograms.js
+++ frontend/src/components/Histograms.js
@@ -3,7 +3,7 @@ import * as d3 from "d3";
 
 export default () => {
     const d3Container = useRef(null); //useRef hook creates a variable that holds on to a value throughout rendering
-    const data = [4, 8, 15, 17, 24, 4];
+    const data = [{ grade: "9", value: 1 }, { grade: "10a", value: 3 }, { grade: "10b", value: 4 }, { grade: "10c", value: 6 }, { grade: "10d", value: 3 }, { grade: "11a", value: 2 }, { grade: "11b", value: 3 }, { grade: "11c", value: 2 }, { grade: "11d", value: 1 }, { grade: "12a", value: 1 }];
 
     useEffect(() => {
         if (data && d3Container.current) {
@@ -12,9 +12,32 @@ export default () => {
     }, [data, d3Container.current])
 
     const drawBarChart = (data) => {
-        const height = 400;
+        const barHeight = 20;
+        const margin = ({ top: 30, right: 30, bottom: 30, left: 30 });
+        const height = Math.ceil((data.length + 0.1) * barHeight) + margin.top + margin.bottom;
         const width = 600;
-        const scale = 5;
+      
+        const x = d3.scaleLinear()
+            .domain([0, d3.max(data, d => d.value)])
+            .range([margin.left, width - margin.right]);
+        
+        const y = d3.scaleBand()
+            .domain(d3.range(data.length))
+            .rangeRound([margin.top, height - margin.bottom])
+            .padding(0.1);
+
+        // const xAxis = g => g
+        //     .attr("transform", `translate(0,${margin.top})`)
+        //     .call(d3.axisTop(x).ticks(width / 80, data.format))
+        //     .call(g => g.select(".domain").remove());
+
+        const yAxis = g => g
+            .attr("transform", `translate(${margin.left}, 0)`)
+            .call(d3.axisLeft(y).tickFormat(i => data[i].grade).tickSizeOuter(0));
+
+        
+        const format = x.tickFormat(1, data.format);
+
         const svgCanvas = d3.select(d3Container.current)
             .append("svg")
             .attr("viewBox", [0, 0, width, height])
@@ -27,25 +50,30 @@ export default () => {
             .join("rect")
                 .attr("x", x(0))
                 .attr("y", (d, i) => y(i))
-                .attr("width", d => x(d) - x(0))
+                .attr("width", d => x(d.value) - x(0))
                 .attr("height", y.bandwidth());
 
+        // Uncomment the below to add values (floats) to bars
         svgCanvas.append("g")
-            .call(xAxis);
+            .attr("fill", "red")
+            .attr("text-anchor", "end")
+            .attr("font-family", "san-serif")
+            .attr("font-size", 12)
+            .selectAll("text")
+            .data(data)
+            .join("text")
+                .attr("x", d => x(d.value) + 10)
+                .attr("y", (d, i) => y(i) + y.bandwidth() / 2)
+                .attr("dy", "0.35em")
+                .text(d => format(d.value));
+
+        // svgCanvas.append("g")
+        //     .call(xAxis);
 
         svgCanvas.append("g")
             .call(yAxis);
 
         return svgCanvas.node();
-        
-        // svgCanvas.selectAll("rect")
-        //     .data(data).enter()
-        //     .append("rect")
-        //     .attr("width", 30) //width of bars
-        //     .attr("height", (datapoint) => datapoint * scale)
-        //     .attr("fill", "orange")
-        //     .attr("x", (datapoint, iteration) => iteration * 35)
-        //     .attr("y", (datapoint) => canvasHeight - datapoint * scale)
     }
 
     return (
