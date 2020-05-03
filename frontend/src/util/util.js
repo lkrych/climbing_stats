b14f8f4ed1690a897eddd725965e300197da38c5
Leland Krych <leland.krych@gmail.com>
2020-05-03 14:47:13 -0700

    add some helpful utilities

--- frontend/src/util/util.js
+++ frontend/src/util/util.js
@@ -2,3 +2,14 @@
 export const range = (size, startAt = 0) => {
     return [...Array(size).keys()].map(i => i + startAt);
 }
+
+export const rangeWithChars = (size, startAt = 0, chars) => {
+    let gradedClimbs = [];
+    let gradeRange = range(size, startAt);
+    gradeRange.forEach(grade => {
+        chars.forEach(char => {
+            gradedClimbs.push(`${grade}${char}`)
+        });
+    })
+    return gradedClimbs;
+}
