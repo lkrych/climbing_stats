6829322b9e5bbe1f037b1440e3a77cd05824330d
Leland Krych <leland.krych@gmail.com>
2020-05-10 18:25:58 -0700

    organize and sort climbs

--- frontend/src/util/util.js
+++ frontend/src/util/util.js
@@ -15,3 +15,35 @@ export const rangeWithChars = (size, startAt = 0, chars) => {
     })
     return gradedClimbs;
 }
+
+export const explodeClimbsObject = (obj, type) => {
+    let objArray = [];
+    Object.keys(obj).forEach(key => {
+        objArray.push({
+            grade: key,
+            value: obj[key]
+        });
+    });
+    if (type == "boulder") {
+        objArray.sort((a, b) => parseInt(a['grade'].substring(1)) - parseInt(b['grade'].substring(1)));
+    } else {
+        objArray.sort((a, b) => {
+            if (a.length < 2 && b.length < 2) {
+                return parseInt(a) - parseInt(b);
+            } else if (a.length < 2) {
+                return -1
+            } else if (b.length < 2) {
+                return 1
+            } else {
+                let aGrade = parseInt(a['grade'].substring(0,2));
+                let bGrade = parseInt(b['grade'].substring(0,2));
+                if (aGrade == bGrade) {
+                    a['grade'].charCodeAt(0) - b['grade'].charCodeAt(0);
+                } else {
+                    return aGrade - bGrade
+                }
+            }
+        })
+    }
+    return objArray;
+}
