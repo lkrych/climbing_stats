3b7c084305fcd240804ccedd327553d0ee04924b
Tatiana Faramarzi <tfaramarzi@gmail.com>
2020-05-21 22:40:06 -0700

    Add styled components to Splash and Nav

--- frontend/src/components/Splash.js
+++ frontend/src/components/Splash.js
@@ -2,25 +2,32 @@ import React, { Fragment } from "react";
 import { NavLink } from "react-router-dom";
 
 import { Container, Header } from 'semantic-ui-react';
-import { Button, MainHeader } from '../styled/styled_components';
+import { Button, MainHeader, SubHeader, Text } from '../styled/styled_components';
 
 export default ({ setShowSignup }) => {
     return ( 
         <Fragment>
             <div className="background-container">
                 <Container fluid >
-                    <MainHeader>
-                        Reach your climbing goals with Pinnacle
-                    </MainHeader>
-                    <h2 className="splash-secondary-desktop">
-                        Simple logging for your climbs
-                    </h2>
-                    <NavLink to="/signup">
-                        <Button 
-                            onClick={() => setShowSignup(false)}>
-                            Sign up
-                        </Button>
-                    </NavLink>
+                    <div className="splash-content">
+                        <MainHeader>
+                            Reach your climbing goals with Pinnacle
+                        </MainHeader>
+                        <SubHeader>
+                                Simple logging for your gym climbs
+                        </SubHeader>
+                        <NavLink to="/signup">
+                            <Button
+                                onClick={() => setShowSignup(false)}>
+                                Sign up
+                            </Button>
+                        </NavLink>
+                    </div>
+                    <div className="about">
+                        <Text>
+                            A description of this service.
+                        </Text>
+                    </div>
                 </Container>
             </div>
         </Fragment>
