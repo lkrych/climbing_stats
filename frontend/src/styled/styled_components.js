2fb259b51ddb89625f8d52d7af4714c13b5e9d0f
Tatiana Faramarzi <tfaramarzi@gmail.com>
2020-05-21 22:38:07 -0700

    Build on styled components and css

--- frontend/src/styled/styled_components.js
+++ frontend/src/styled/styled_components.js
@@ -7,11 +7,20 @@ export const Button = styled.button`
     font-size: 1.25rem;
     color: #f9cd0e;
     padding: 1rem 2rem;
-    margin: 1rem;
+    margin: 0 auto;
     cursor: pointer;
+    transition: font-size .5s linear;
+    -o-transition: font-size .25s linear; 
+    -moz-transition: font-size .25s linear; 
+    -webkit-transition: font-size .25s linear; 
 
     &:hover {
         background: white;
+    }
+
+    @media (max-width: 600px) {
+        font-size: 1rem;
+        padding: .75rem 1.75rem;
     }
 `
 
@@ -19,5 +28,17 @@ export const MainHeader = styled.h1`
     font-size: 6rem;
     font-weight: normal;
     margin-bottom: 0;
+    color: #f9cd0e;
+`
+
+export const SubHeader = styled.h2`
+    font-size: 3rem;
+    font-weight: normal;
+    color: white;
+`
+
+export const Text = styled.p`
+    font-size: 2rem;
+    font-weight: normal;
     color: white;
 `
