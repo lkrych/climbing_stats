b2a05fd828002391b048fa3e61422a9dd5263de5
Tatiana Faramarzi <tfaramarzi@gmail.com>
2020-05-21 21:37:17 -0700

    Add styled components file

---
+++ frontend/src/styled/styled_components.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+    border-radius: 2px;
+    border: 2px solid #f9cd0e;
+    background-color: Transparent;
+    font-size: 1.25rem;
+    color: #f9cd0e;
+    padding: 1rem 2rem;
+    margin: 1rem;
+    cursor: pointer;
+
+    &:hover {
+        background: white;
+    }
+`
+
+export const MainHeader = styled.h1`
+    font-size: 6rem;
+    font-weight: normal;
+    margin-bottom: 0;
+    color: white;
+`
