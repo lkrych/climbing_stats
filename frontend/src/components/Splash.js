1c9d409e8b68aa82da9d3c341f3c93df489aeab5
Tatiana Faramarzi <tfaramarzi@gmail.com>
2020-05-21 21:35:53 -0700

    Add styled components to splash page

--- frontend/src/components/Splash.js
+++ frontend/src/components/Splash.js
@@ -1,30 +1,24 @@
 import React, { Fragment } from "react";
 import { NavLink } from "react-router-dom";
-import { Container, Header, Icon, Button } from 'semantic-ui-react'
-import "../../index.css";
 
-export default ({ setShowSignup, mobile }) => {
-    return (
+import { Container, Header } from 'semantic-ui-react';
+import { Button, MainHeader } from '../styled/styled_components';
+
+export default ({ setShowSignup }) => {
+    return ( 
         <Fragment>
             <div className="background-container">
                 <Container fluid >
-                    <h1 className="splash-header-desktop">
-                        Climbing Stats
-                    </h1>
+                    <MainHeader>
+                        Reach your climbing goals with Pinnacle
+                    </MainHeader>
                     <h2 className="splash-secondary-desktop">
                         Simple logging for your climbs
                     </h2>
                     <NavLink to="/signup">
                         <Button 
-                            onClick={() => setShowSignup(false)} 
-                            color='orange' 
-                            size='huge'
-                            style={{
-                                marginLeft: '10vw'
-                            }}
-                        >
+                            onClick={() => setShowSignup(false)}>
                             Sign up
-                            <Icon name='right arrow' />
                         </Button>
                     </NavLink>
                 </Container>
